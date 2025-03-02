d5af43e75468812807faeb412e6ac2c0ef322ab1
Lourdes Jiménez <00090220@uca.edu.sv>
2025-03-02 14:34:30 -0600

    Add templates files

---
+++ src/funciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "../include/prueba.h"
+
+using namespace std;
+
+string imprimirPiramide(int altura){
+    //En esta función se debe implementar la lógica para imprimir el triangulo y debe retornar un string  
+}
+
+#ifndef TESTING 
+int main(){
+    
+    return 0;
+}
+#endif
